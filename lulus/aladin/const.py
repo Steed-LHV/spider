3768e16121bb1001dec1110a7330e1b03f3277f8
your_username <2018646450@qq.com>
2016-06-04 09:43:12 +0800

    lulus

--- lulus/aladin/const.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-#coding=utf-8
-
-"""
-常量定义文件
-"""
-
-
--- lulus/aladin/mail.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#coding=utf-8
-
-from flask.ext.mail import Mail
-
-
-__all__ = ['mail']
-
-mail = Mail()
